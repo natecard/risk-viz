850632d6b2c45d4b6646fb4163a39352b5cf0b83
Nate Card <ncard24@gmail.com>
2023-04-22 16:59:45 -0400

    Initial commit, JSON route set up

---
+++ src/app/api/data/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server";
+
+const sheetsId = process.env.GOOGLE_SHEETS_ID!;
+const apiKey = process.env.GOOGLE_SHEETS_API!;
+export async function GET(){
+const response = await fetch(`https://sheets.googleapis.com/v4/spreadsheets/${sheetsId}/values/Sheet1?alt=json&key=${apiKey}`)
+const data = await response.json()
+return NextResponse.json({ data })
+}
