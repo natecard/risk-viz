f36c98fccbe4532430305b25ae9bb88e5f536f5c
Nate Card <ncard24@gmail.com>
2023-04-24 10:34:21 -0400

    React suspense and prefetch enabled for data fetch

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,56 +1,58 @@
 'use client'
 import { Map, Layer, Source, LayerProps, MapRef } from "react-map-gl";
-import { useEffect, useState, useRef } from "react";
-import { GeoJSON, Feature } from '../api/data/route';
-const apiKey: string = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN!
+import { useEffect, useState, useRef, Suspense } from 'react';
+import { Feature, GeoJSON } from '../interface';
+import Loading from './loading';
+const apiKey: string = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN!;
 
-  
-export default async function DataMap(){
-console.log("DataMap rendered");
- const [data, setData] = useState<GeoJSON | any | null>(null)
- const mapRef = useRef<MapRef|null>(null);
-//lat and lng set to Toronto on initialization
-//TODO Set up map data layer and points
-useEffect(() => {
-  getData();
-  console.log(mapRef);
-}, []);
-useEffect(() => {
-  getData();
-}, []);
+export default async function DataMap() {
+  // console.log('DataMap rendered');
+  const [data, setData] = useState();
+  const mapRef = useRef<MapRef | null>(null);
+  //lat and lng set to Toronto on initialization
+  //TODO Set up map data layer and points
+  useEffect(() => {
+    getData();
+    console.log(mapRef);
+  }, []);
+  const getData = () => {
+    fetch('/api/data/')
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => console.log(error));
+  };
 
-const getData = () => {
-  fetch("/api/data/")
-    .then((response) => response.json())
-    .then((data) => setData(data))
-    .catch((error) => console.log(error));
-};
- console.log(data);
- const layerData: LayerProps = {
-   id: 'Point',
-   type: 'circle',
-   paint: {
-     'circle-radius': 10,
-     'circle-color': '#007cbf',
-   },
- };
+  const layerData: LayerProps = {
+    id: 'Point',
+    type: 'circle',
+    paint: {
+      'circle-radius': 5,
+      'circle-color': '#007cbf',
+    },
+  };
 
-    return (
+  return (
+    <div>
+      {/* {data && ( */}
+      <Suspense fallback={<Loading />}>
         <Map
-            ref={mapRef}
-            reuseMaps
-            initialViewState={{
+          ref={mapRef}
+          reuseMaps
+          initialViewState={{
             longitude: -79.36,
             latitude: 43.65,
-            zoom: 6}}
-            mapStyle="mapbox://styles/mapbox/light-v11"
-            style={{width: "100vw", height: "60vh"}}
-            mapboxAccessToken={apiKey}
+            zoom: 6,
+          }}
+          mapStyle='mapbox://styles/mapbox/light-v11'
+          style={{ width: '100vw', height: '60vh' }}
+          mapboxAccessToken={apiKey}
         >
-            {data && (
-            <Source type="geojson" data={data}>
-                <Layer {...layerData} />
-            </Source>)}
+          <Source type='geojson' data={'http://localhost:3000/api/data'}>
+            <Layer {...layerData} />
+          </Source>
         </Map>
-    );
+      </Suspense>
+      {/* )} */}
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,20 @@ import Link from "next/link";
 
 export default async function Home() {
   return (
-    <div className="">
-      
-      <main className="py-72 flex flex-row justify-center">
-        <div className="px-8">
-          <Link href="/map">
+    <div className=''>
+      <main className='flex flex-row justify-center py-72'>
+        <div className='px-8'>
+          <Link prefetch href='/map'>
             <button>Data Map</button>
           </Link>
         </div>
-        <div className="px-8">
-          <Link href="/table">
+        <div className='px-8'>
+          <Link href='/table'>
             <button>Data Table</button>
           </Link>
         </div>
-        <div className="px-8">
-          <Link href="/chart">
+        <div className='px-8'>
+          <Link href='/chart'>
             <button>Data Chart</button>
           </Link>
         </div>
