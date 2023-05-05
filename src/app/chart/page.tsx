615ae3cdbd474945854b1c8572c735945bad9e2b
Nate Card <ncard24@gmail.com>
2023-05-05 13:52:30 -0700

    props changed for passing data

--- src/app/chart/page.tsx
+++ src/app/chart/page.tsx
@@ -1,13 +1,4 @@
-import { FeatureCollection } from 'geojson';
 import ChartComponent from './Components/ChartComponent';
-import { convertJSON, convertToGeoJSON } from '../jsonToGeo';
-
-const response = await fetch(process.env.NEXT_PUBLIC_WEB_URL! + '/api/data', {
-  cache: 'no-cache',
-});
-const data = await response.json();
-const jsonData = convertJSON(data);
-const geoData = convertToGeoJSON(jsonData);
 
 export default function Chart() {
   return (
